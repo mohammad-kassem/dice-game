2d4002b27e69f6d56bb65154212da74108a72161
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-23 11:14:08 +0300

    adding minor changes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function playDice(){
-    let dice_number_palyer1 =  Math.floor(Math.random() * 6) + 1;
-    let dice_number_palyer2 =  Math.floor(Math.random() * 6) + 1;
-    return [dice_number_palyer1, dice_number_palyer2];
+function generateDiceNumbers(){
+    let palyer1_dice_number =  Math.floor(Math.random() * 6) + 1;
+    let player2_dice_number =  Math.floor(Math.random() * 6) + 1;
+    return [palyer1_dice_number, player2_dice_number];
 
 }
 
-function displayDice(dice_number_palyer1, dice_number_player2){
+function displayDice(player1_dice_number, player2_dice_number){
     image = document.getElementsByTagName("img")[0];
-    image.src = `./images/dice${dice_number_palyer1}.png`;
+    image.src = `./images/dice${player1_dice_number}.png`;
     image = document.getElementsByTagName("img")[1];
-    image.src = `./images/dice${dice_number_player2}.png`;
+    image.src = `./images/dice${player2_dice_number}.png`;
 }
 
-function gameStatus(player1_number, player2_number){
+function gameStatus(player1_dice_number, player2_dice_number){
     let result = document.getElementsByTagName("h1")[0];
     let result_message = ""
-    if (player1_number > player2_number){
+    if (player1_dice_number > player2_dice_number){
         result_message = "🚩 Play 1 Wins!";
 
     } 
-    else if (player1_number < player2_number){
+    else if (player1_dice_number < player2_dice_number){
         result_message = "🚩 Play 2 Wins!";
 
     }
@@ -29,10 +29,10 @@ function gameStatus(player1_number, player2_number){
 
 }
 
-function game(){
-    dice_number = playDice();
+function playDice(){
+    dice_number = generateDiceNumbers();
     displayDice(dice_number[0], dice_number[1]);
     gameStatus(dice_number[0], dice_number[1]);
 }
 
-game();
+playDice();
